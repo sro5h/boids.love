ebeeb5f94319f0724de44e6efcce4bc10667a956
Paul Meffle <meffle.paul@me.com>
2016-12-13 22:20:07 +0100

    add main.lua with sample code

---
+++ main.lua
@@ -0,0 +1,16 @@
+-- constants
+SCREEN_SIZE = 600;
+
+function love.load()
+    -- initial graphics setup
+    love.graphics.setBackgroundColor(27, 32, 42);
+    love.window.setMode(SCREEN_SIZE, SCREEN_SIZE);
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
