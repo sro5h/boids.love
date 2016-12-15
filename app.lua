299472a6a84ab9de3425e617872b5f00584a1c8d
Paul Meffle <meffle.paul@me.com>
2016-12-15 22:00:06 +0100

    add player & add boid at random position if none was passed

--- app.lua
+++ app.lua
@@ -6,7 +6,9 @@ local app_mt = {__index = app};
 -- constants
 app.EPSILON = 0.0000001;
 app.CIRCLE_SEGMENTS = 30;
-app.DEFAULT_COLOR = { 96, 140, 195};
+app.DEFAULT_COLOR = {96, 140, 195};
+app.PLAYER_SIZE = 10;
+app.BOID_SIZE = 10;
 
 -- creates new app
 function app.new(width, height)
@@ -14,8 +16,12 @@ function app.new(width, height)
         _width = width,
         _height = height,
 
+        _player = {},
         _boids = {}
     }, app_mt);
+
+    -- create player
+    _app._player = boid.new(_app._width / 2, _app._height / 2, app.PLAYER_SIZE, {177, 94, 124});
 
     -- initial graphics setup
     love.window.setMode(_app._width, _app._height);
@@ -36,6 +42,12 @@ local function drawBoid(b)
     love.graphics.circle("line", b:getX(), b:getY(), b:getSize(), app.CIRCLE_SEGMENTS);
 end
 
+-- draw a player
+local function drawPlayer(p)
+    love.graphics.setColor(unpack(p:getColor()));
+    love.graphics.circle("line", p:getX(), p:getY(), p:getSize(), app.CIRCLE_SEGMENTS);
+end
+
 -- get the width
 function app:getWidth()
     return self._width;
@@ -48,22 +60,31 @@ end
 
 -- add a boid to _boids
 function app:addBoid(b)
-    self._boids[#self._boids + 1] = b;
+    if(not b) then
+        local posX = math.random(0, self._width);
+        local posY = math.random(0, self._height);
+        self:addBoid(boid.new(posX, posY, app.BOID_SIZE));
+    else
+        self._boids[#self._boids + 1] = b;
+    end
+end
+
+-- init
+function app:init()
+    math.randomseed(os.time());
+    for i = 1, 10 do
+        myApp:addBoid();
+    end
 end
 
 -- handleEvents
 function app:handleInput()
+    self._player:setX(love.mouse.getX());
+    self._player:setY(love.mouse.getY());
+
+    -- key presses
     if(love.keyboard.isDown("a")) then
-        print("a pressed");
-    end
-    if(love.keyboard.isDown("w")) then
-        print("w pressed");
-    end
-    if(love.keyboard.isDown("d")) then
-        print("d pressed");
-    end
-    if(love.keyboard.isDown("s")) then
-        print("s pressed");
+        self:addBoid();
     end
 end
 
@@ -77,6 +98,8 @@ function app:draw()
     for key, b in pairs(self._boids) do
         drawBoid(b);
     end
+
+    drawPlayer(self._player);
 end
 
 return app;
--- main.lua
+++ main.lua
@@ -3,12 +3,11 @@ local boid = require("boid");
 
 -- constants
 SCREEN_SIZE = 600;
-BOID_SIZE = 10;
 
 function love.load()
     -- initial graphics setup
     myApp = app.new(SCREEN_SIZE, SCREEN_SIZE);
-    myApp:addBoid(boid.new(40, 50, BOID_SIZE, {177, 94, 124}));
+    myApp:init();
 end
 
 function love.update(dt)
